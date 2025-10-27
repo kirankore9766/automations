ebebf8dc3ba7d1fd82ab34e5545c92512b1ea189
kirankore9766 <kiran.kore@cointab.net>
2025-10-27 12:29:55 +0530

    swap twoNum.js

---
+++ swap twoNum.js
@@ -0,0 +1,8 @@
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+//using destructuring assignment
+[a, b] = [b, a];
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
