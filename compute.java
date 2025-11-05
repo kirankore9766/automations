3b5346b78d2d5cb9c520c1196faa3a1dc55ee588
kirankore9766 <kiran.kore@cointab.net>
2025-11-05 11:29:34 +0530

    Update compute.java

--- compute.java
+++ compute.java
@@ -3,11 +3,13 @@ public class QuotientRemainder {
   public static void main(String[] args) {
 
     int dividend = 25, divisor = 4;
+    int divided = 25, divisor =5;
 
     int quotient = dividend / divisor;
     int remainder = dividend % divisor;
 
     System.out.println("Quotient = " + quotient);
     System.out.println("Remainder = " + remainder);
+    System.out.println("Remainder = " + remainders);
   }
 }
